70899d613e03491777f143cd8241008d1bcdc797
Jiideal <18356523272@163.com>
2024-05-31 00:40:47 +0800

    -

---
+++ assets/views_Vue_Vue3Plugins_Echarts_lineChart.md.DcX0ehba.js
@@ -0,0 +1,81 @@
+import{_ as s,c as n,o as a,a4 as p}from"./chunks/framework.Cq70EfMF.js";const l="/iDeal-Press/assets/%E6%8A%98%E7%BA%BF%E5%9B%BE%E7%A4%BA%E6%84%8F%E5%9B%BE.DS6ylRSk.png",m=JSON.parse('{"title":"简易折线图","description":"","frontmatter":{},"headers":[],"relativePath":"views/Vue/Vue3Plugins/Echarts/lineChart.md","filePath":"views/Vue/Vue3Plugins/Echarts/lineChart.md"}'),e={name:"views/Vue/Vue3Plugins/Echarts/lineChart.md"},t=p('<h1 id="简易折线图" tabindex="-1">简易折线图 <a class="header-anchor" href="#简易折线图" aria-label="Permalink to &quot;简易折线图&quot;">​</a></h1><p><a href="https://echarts.apache.org/examples/zh/index.html" target="_blank" rel="noreferrer">echarts官网</a></p><p><img src="'+l+`" alt="效果图"></p><ol><li>安装<code>echarts</code></li></ol><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>pnpm install echarts;</span></span></code></pre></div><ol start="2"><li>页面中使用</li></ol><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>&lt;template&gt;</span></span>
+<span class="line"><span>  &lt;div id=&quot;myEcharts&quot; class=&quot;myEcharts&quot;&gt;&lt;/div&gt;</span></span>
+<span class="line"><span>&lt;/template&gt;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>&lt;script setup&gt;</span></span>
+<span class="line"><span>import {</span></span>
+<span class="line"><span>  onMounted,</span></span>
+<span class="line"><span>  onUnmounted</span></span>
+<span class="line"><span>} from &#39;vue&#39;;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>import * as echarts from &quot;echarts&quot;;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>let echart = echarts;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>// 基础配置一下Echarts</span></span>
+<span class="line"><span>function initChart() {</span></span>
+<span class="line"><span>  let chart = echart.init(document.getElementById(&quot;myEcharts&quot;), &quot;dark&quot;);</span></span>
+<span class="line"><span>  // 把配置和数据放这里</span></span>
+<span class="line"><span>  chart.setOption({</span></span>
+<span class="line"><span>    xAxis: {</span></span>
+<span class="line"><span>      type: &quot;category&quot;,</span></span>
+<span class="line"><span>      data: [</span></span>
+<span class="line"><span>        &quot;一月&quot;,</span></span>
+<span class="line"><span>        &quot;二月&quot;,</span></span>
+<span class="line"><span>        &quot;三月&quot;,</span></span>
+<span class="line"><span>        &quot;四月&quot;,</span></span>
+<span class="line"><span>        &quot;五月&quot;,</span></span>
+<span class="line"><span>        &quot;六月&quot;,</span></span>
+<span class="line"><span>        &quot;七月&quot;,</span></span>
+<span class="line"><span>        &quot;八月&quot;,</span></span>
+<span class="line"><span>        &quot;九月&quot;,</span></span>
+<span class="line"><span>        &quot;十月&quot;,</span></span>
+<span class="line"><span>        &quot;十一月&quot;,</span></span>
+<span class="line"><span>        &quot;十二月&quot;</span></span>
+<span class="line"><span>      ]</span></span>
+<span class="line"><span>    },</span></span>
+<span class="line"><span>    tooltip: {</span></span>
+<span class="line"><span>      trigger: &quot;axis&quot;</span></span>
+<span class="line"><span>    },</span></span>
+<span class="line"><span>    yAxis: {</span></span>
+<span class="line"><span>      type: &quot;value&quot;</span></span>
+<span class="line"><span>    },</span></span>
+<span class="line"><span>    series: [</span></span>
+<span class="line"><span>      {</span></span>
+<span class="line"><span>        data: [</span></span>
+<span class="line"><span>          820,</span></span>
+<span class="line"><span>          932,</span></span>
+<span class="line"><span>          901,</span></span>
+<span class="line"><span>          934,</span></span>
+<span class="line"><span>          1290,</span></span>
+<span class="line"><span>          1330,</span></span>
+<span class="line"><span>          1320,</span></span>
+<span class="line"><span>          801,</span></span>
+<span class="line"><span>          102,</span></span>
+<span class="line"><span>          230,</span></span>
+<span class="line"><span>          4321,</span></span>
+<span class="line"><span>          4129</span></span>
+<span class="line"><span>        ],</span></span>
+<span class="line"><span>        type: &quot;line&quot;,</span></span>
+<span class="line"><span>        smooth: true, // 线条平滑</span></span>
+<span class="line"><span>      }</span></span>
+<span class="line"><span>    ]</span></span>
+<span class="line"><span>  });</span></span>
+<span class="line"><span>  window.onresize = function () {</span></span>
+<span class="line"><span>    //自适应大小</span></span>
+<span class="line"><span>    chart.resize();</span></span>
+<span class="line"><span>  };</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>onMounted(() =&gt; {</span></span>
+<span class="line"><span>  initChart();</span></span>
+<span class="line"><span>});</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>&lt;/script&gt;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>&lt;style&gt;</span></span>
+<span class="line"><span>.myEcharts {</span></span>
+<span class="line"><span>  width: 500px;</span></span>
+<span class="line"><span>  height: 500px;</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span>&lt;/style&gt;</span></span></code></pre></div>`,7),i=[t];function c(o,r,u,h,d,q){return a(),n("div",null,i)}const _=s(e,[["render",c]]);export{m as __pageData,_ as default};
